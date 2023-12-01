7590dc08efd121192ac4315d798058841693b5b7
Deepak Kumar <k.deepak2217@gmail.com>
2023-12-01 12:36:11 +0000

    updated

---
+++ variables.js
@@ -0,0 +1,13 @@
+const accountId = 14455;
+let accountEmail = "deepak05@gmail.com";
+var accountPassword = "1235676";
+let accountCity = "Noida";
+let accountState
+
+/*
+Prefer not to use reassign with const variable
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+console.log(accountEmail);
+console.warn()
